5d66b7b70922e726669b87efd1a4a4e23668c19c
pmetodiev <100308573+pmetodiev@users.noreply.github.com>
2022-03-13 12:34:38 +0200

    example

---
+++ mathematics.java
@@ -0,0 +1,13 @@
+package math;
+
+
+public class mathematics {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-147));
+        System.out.println(Math.abs(15));
+        System.out.println(Math.abs(0));
+        System.out.println(Math.abs(18));
+        System.out.println("147 \t 15 \t 0 \t 18");
+    }
+}
---
+++ name.java
@@ -0,0 +1,10 @@
+package name;
+
+public class name {
+    public static void main(String[] args) {
+        String myName = "Plamen";
+        System.out.println(myName.length());
+        System.out.println(myName.toUpperCase());
+        System.out.println(myName.toLowerCase());
+    }
+}
